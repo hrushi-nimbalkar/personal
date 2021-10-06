479895b99e460580c048d8062a24261f98b6ccf0
hrushikesh <hrushikeshnimbalkarhmn1@gmail.com>
2021-10-06 15:10:36 +0530

    added resume.c

---
+++ add n numbers using array.c
@@ -0,0 +1,17 @@
+//add n numbers
+#include<stdio.h>
+void main()
+{
+	int n,i,addition;
+	printf("enter how many numbers");
+	scanf("%d",&n);
+	addition=num[1];
+	for(i=0;i+n;i++);
+	{
+		if(sum=i+n; i++);
+		{
+			addition=num [1]
+		}
+		printf("addition is %d",addition)
+	}
+}
